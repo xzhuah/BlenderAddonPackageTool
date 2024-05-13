1d29ba527a4853c814799ef57c50e4ebbd0b2d86
ZHU Xinyu <xzhuah77@gmail.com>
2024-05-12 21:46:34 -0700

    Add MIT License

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-import atexit
-import os
-import re
-import shutil
-import subprocess
+# MIT License
+#
+# Copyright (c) [2024] [Xinyu Zhu]
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import atexit, os, re, shutil, subprocess, time, threading
 from pathlib import Path
 from datetime import datetime
-import time
 from os import listdir
 from os.path import isfile, isdir
-import threading
 
 from common.class_loader.module_installer import install_if_missing, install_fake_bpy, default_blender_addon_path
 
