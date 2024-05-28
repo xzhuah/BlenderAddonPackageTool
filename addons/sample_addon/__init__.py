0d1d63d990a768c18db691eaff6ef4b651cd8bc3
ZHU Xinyu <xzhuah77@gmail.com>
2024-05-27 18:57:44 -0700

    in sample addon, provide example of declaring properties

--- addons/sample_addon/__init__.py
+++ addons/sample_addon/__init__.py
@@ -3,6 +3,7 @@ import bpy
 from common.class_loader import auto_load
 from addons.sample_addon.config import __addon_name__
 from addons.sample_addon.i18n.dictionary import dictionary
+from common.class_loader.auto_load import add_properties, remove_properties
 from common.i18n.dictionary import common_dictionary
 from common.i18n.i18n import load_dictionary
 
@@ -20,12 +21,22 @@ bl_info = {
     "category": "3D View"
 }
 
+_addon_properties = {}
+
+
+# You may declare properties like following, framework will automatically add and remove them.
+# _addon_properties = {
+#     bpy.types.Scene: {
+#         "property_name": bpy.props.StringProperty(name="property_name"),
+#     },
+# }
 
 def register():
     print("registering")
     # Register classes
     auto_load.init()
     auto_load.register()
+    add_properties(_addon_properties)
 
     # Internationalization
     load_dictionary(dictionary)
@@ -39,5 +50,6 @@ def unregister():
     bpy.app.translations.unregister(__addon_name__)
     # unRegister classes
     auto_load.unregister()
+    remove_properties(_addon_properties)
 
     print("{} addon is uninstalled.".format(bl_info["name"]))
