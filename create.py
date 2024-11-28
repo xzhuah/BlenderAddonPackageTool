3937ac6a1e99b6c88846e94c6914bd1c016144b8
ksc <ksc010@qq.com>
2024-11-28 20:56:13 +0800

    移除废弃变量

--- create.py
+++ create.py
@@ -4,8 +4,6 @@ from main import new_addon, ACTIVE_ADDON
 
 # The name of the addon to be created, this name is defined in the config.py of the addon as __addon_name__
 # 插件的config.py文件中定义的插件名称 __addon_name__
-addon_name_to_create = ACTIVE_ADDON
-# addon_name_to_create = "new_addon"
 
 if __name__ == '__main__':
     import argparse
