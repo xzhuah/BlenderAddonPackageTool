83efd0acd452d8ced5c002815c20e6df3c6920b8
ksc <ksc010@qq.com>
2024-11-28 20:59:10 +0800

    修复指定插件名没有生效的问题 完善命令行参数说明

--- release.py
+++ release.py
@@ -4,20 +4,19 @@ from main import get_init_file_path, release_addon, ACTIVE_ADDON
 
 # The name of the addon to be released, this name is defined in the config.py of the addon as __addon_name__
 # 插件的config.py文件中定义的插件名称 __addon_name__
-addon_name_to_release = ACTIVE_ADDON
-# addon_name_to_release = "new_addon"
+
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('addon', default=ACTIVE_ADDON, nargs='?', help='addon name')
-    parser.add_argument('--disable_zip', default=False, action='store_true', help='disable zip package')
-    parser.add_argument('--with_version', default=False, action='store_true', help='append version')
-    parser.add_argument('--with_timestamp', default=False, action='store_true', help='append timestamp')
+    parser.add_argument('--disable_zip', default=False, action='store_true', help='If true, release the addon into a plain folder and do not zip it into an installable package, useful if you want to add more files and zip by yourself.')
+    parser.add_argument('--with_version', default=False, action='store_true', help='Append the addon version number (as specified in bl_info) to the released zip file name.')
+    parser.add_argument('--with_timestamp', default=False, action='store_true', help='Append a timestamp to the zip file name.')
     args = parser.parse_args()
-    release_addon(target_init_file = get_init_file_path(args.addon), 
-                  addon_name = addon_name_to_release,
-                  need_zip= not args.disable_zip,
-                  with_timestamp = args.with_timestamp,
-                  with_version = args.with_version,
+    release_addon(target_init_file=get_init_file_path(args.addon), 
+                  addon_name=args.addon,
+                  need_zip=not args.disable_zip,
+                  with_timestamp=args.with_timestamp,
+                  with_version=args.with_version,
                 )
--- test.py
+++ test.py
@@ -4,13 +4,11 @@ from main import test_addon, ACTIVE_ADDON
 
 # The name of the addon to be tested, this name is defined in the config.py of the addon as __addon_name__
 # 插件的config.py文件中定义的插件名称 __addon_name__
-addon_name_to_test = ACTIVE_ADDON
-# addon_name_to_test = "new_addon"
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('addon', default=ACTIVE_ADDON, nargs='?', help='addon name')
-    parser.add_argument('--disable_watch', default=False, action='store_true', help='disable zip package')
+    parser.add_argument('--disable_watch', default=False, action='store_true', help='Do not reload addon when file changed')
     args = parser.parse_args()
-    test_addon(addon_name_to_test, enable_watch = not args.disable_watch)
+    test_addon(args.addon, enable_watch = not args.disable_watch)
