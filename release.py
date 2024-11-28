55ff20ca5470049717f390faf3b8d00537b1b8db
ksc <ksc010@qq.com>
2024-11-28 17:04:33 +0800

    增加--disable_zip 禁用压缩打包

--- release.py
+++ release.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('addon', default=ACTIVE_ADDON, nargs='?', help='addon name')
-    parser.add_argument('--need_zip', default=True, action='store_true', help='need zip package')
+    parser.add_argument('--disable_zip', default=False, action='store_true', help='disable zip package')
     parser.add_argument('--with_version', default=False, action='store_true', help='append version')
     parser.add_argument('--with_timestamp', default=False, action='store_true', help='append timestamp')
     args = parser.parse_args()
     release_addon(target_init_file = get_init_file_path(args.addon), 
                   addon_name = addon_name_to_release,
-                  need_zip= args.need_zip,
+                  need_zip= not args.disable_zip,
                   with_timestamp = args.with_timestamp,
                   with_version = args.with_version,
                 )
