13171a0c0d09d15ef2e3a9bd45522d742ac8244d
ksc <ksc010@qq.com>
2024-11-29 10:44:18 +0800

    fix codestyle

--- test.py
+++ test.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     parser.add_argument('addon', default=ACTIVE_ADDON, nargs='?', help='addon name')
     parser.add_argument('--disable_watch', default=False, action='store_true', help='Do not reload addon when file changed')
     args = parser.parse_args()
-    test_addon(args.addon, enable_watch = not args.disable_watch)
+    test_addon(args.addon, enable_watch=not args.disable_watch)
